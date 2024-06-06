60640c77a2c1dbf5539476ea0bee632d6efffdd8
Darnion <Darnion@DESKTOP-PIQMCKV>
2024-06-06 05:41:24 +0300

    Application Done

--- RestMan.UI/Forms/MainForm.cs
+++ RestMan.UI/Forms/MainForm.cs
@@ -122,7 +122,7 @@ namespace RestMan.UI.Forms
                     db.SaveChanges();
                 }
 
-                this.Close();
+                Application.Exit();
             }
         }
 
